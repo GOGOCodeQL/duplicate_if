4c385ebb9a9c081e63d197fd4a3dc112e64061c7
재영 정 <jjy600901@snu.ac.kr>
2020-10-21 19:11:46 +0900

    if

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+func main() {
+	one := 1
+	if one == 1 {
+		println("A") //!
+	} else if one == 1 {
+		println("B")
+	} else if one == 1 {
+		println("C")
+	} else if one = 2; one == 2 {
+		println("D")
+	}
+	two := 2
+	if two = 1; two == two {
+		println("E") //!
+	} else if two = 1; two == two {
+		println("F") //!
+	}
+
+	if two = 1; two == two {
+		println("F")
+	}
+
+	println("ByeBye")
+}
